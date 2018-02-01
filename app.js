ef430843d22a3bc6606df26efccd56ef137f1f05
Luke Champ <luke.champ@outlook.com>
2018-02-01 13:11:27 -0500

    set up bootstrap, firebase, jquery, and local links in html.  Also set up firebase in js

---
+++ app.js
@@ -0,0 +1,15 @@
+// Firebase setup
+
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBZosg-4AFihlcTbmS5hHYtIlN3nCDJclM",
+    authDomain: "proximity-b5d7b.firebaseapp.com",
+    databaseURL: "https://proximity-b5d7b.firebaseio.com",
+    projectId: "proximity-b5d7b",
+    storageBucket: "",
+    messagingSenderId: "476829292964"
+  };
+  firebase.initializeApp(config);
+  var database = firebase.database();
+  
